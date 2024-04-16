cbef0b5582194d0039ae3a3d431100ab7f327c86
917wiszniewskibianca <91844338+917wiszniewskibianca@users.noreply.github.com>
2024-04-16 18:14:58 +0300

    Update AppTest.java

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,38 +1,45 @@
-package ssvv.example;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-import junit.framework.Test;
+import domain.Student;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import repository.StudentXMLRepository;
+import validation.StudentValidator;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+    private StudentXMLRepository studentRepository;
+
+    public StudentTest() {
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Before
+    public void setUp() {
+        this.studentRepository = new StudentXMLRepository(new StudentValidator(), "students.xml");
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testSaveStudent_ValidInput() {
+        Student student = new Student("123", "John Doe", 500);
+        Student savedStudent1 = (Student)this.studentRepository.save(student);
+        TestCase.assertNotNull(savedStudent1);
+        Student savedStudent = (Student)this.studentRepository.findOne("123");
+        TestCase.assertNotNull(savedStudent);
+        Assert.assertEquals("123", savedStudent.getID());
+        Assert.assertEquals("John Doe", savedStudent.getNume());
+        Assert.assertEquals(500L, (long)savedStudent.getGrupa());
+    }
+
+    @Test
+    public void testSaveStudent_InvalidInput() {
+        Student student = new Student("-1", "John Doe", 1000);
+        Student savedStudent1 = (Student)this.studentRepository.save(student);
+        Assert.assertEquals((Object)null, savedStudent1);
+        Student savedStudent = (Student)this.studentRepository.findOne("456");
+        Assert.assertEquals((Object)null, savedStudent);
     }
 }
